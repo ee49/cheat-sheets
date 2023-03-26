0ebfc26db61ac9c6fee8766cf8852f1cdadc1ced
ee49 <mithunpatellu@gmail.com>
2023-03-26 18:39:10 -0400

    Update Other-commands.sh

--- kubernetese/Other-commands.sh
+++ kubernetese/Other-commands.sh
@@ -86,3 +86,12 @@ spec:
 #log into individual container:
 k exec -it podname --container main-app -- /bin/bash
 k log podname # this will print logs from the concerened pods.
+
+
+#Liveness and Readiness:
+#test script:
+for i in {1..20}; do
+   kubectl exec --namespace=kube-public curl -- sh -c 'test=`wget -qO- -T 2  http://webapp-service.default.svc.cluster.local:8080/ready 2>&1` && echo "$test OK" || echo "Failed"';
+   echo ""
+done
+
